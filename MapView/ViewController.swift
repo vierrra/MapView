fa8545b343f5bd9713c3ad7ef0a8ba0654f705dc
Renato <renato.vieira@stant.com.br>
2020-08-05 15:52:30 -0300

    Initial Commit

---
+++ MapView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by Renato on 8/5/20.
+//  Copyright © 2020 Stant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
